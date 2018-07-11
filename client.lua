6781c8b515860ed8012c5fbe4e9237dbf401f3f3
Bacon_Space <thestreamstats@gmail.com>
2018-07-11 14:51:40 -0400

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,72 @@
+
+function SendNotification(options)
+    options.animation = options.animation or {}
+    options.sounds = options.sounds or {}
+    options.docTitle = options.docTitle or {}
+
+    local options = {
+        type = options.type or "info",
+        layout = options.layout or "centerleft",
+        theme = options.theme or "gta",
+        text = options.text or "Empty Notification",
+        timeout = options.timeout or 5000,
+        progressBar = options.progressBar ~= false and true or false,
+        closeWith = options.closeWith or {},
+        animation = {
+            open = options.animation.open or "gta_effects_open",
+            close = options.animation.close or "gta_effects_close"
+        },
+        sounds = {
+            volume = options.sounds.volume or 1,
+            conditions = options.sounds.conditions or {},
+            sources = options.sounds.sources or {}
+        },
+        docTitle = {
+            conditions = options.docTitle.conditions or {}
+        },
+        modal = options.modal or false,
+        id = options.id or false,
+        force = options.force or false,
+        queue = options.queue or "global",
+
+        killer = options.killer or false,
+        container = options.container or false,
+        buttons = options.button or false
+    }
+
+    SendNUIMessage({options = options})
+end
+
+RegisterNetEvent("pNotify:SendNotification")
+AddEventHandler("pNotify:SendNotification", function(options)
+    SendNotification(options)
+end)
+
+-- Shows a notification on the player's screen 
+function ShowNotification( text )
+    SetNotificationTextEntry("STRING")
+    AddTextComponentSubstringPlayerName(text)
+    DrawNotification(false, false)
+end
+
+RegisterCommand('car', function(source, args, rawCommand)
+    local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 8.0, 0.5))
+    local veh = args[1]
+    if veh == nil then veh = "adder" end
+    vehiclehash = GetHashKey(veh)
+    RequestModel(vehiclehash)
+    
+    Citizen.CreateThread(function() 
+        local waiting = 0
+        while not HasModelLoaded(vehiclehash) do
+            waiting = waiting + 100
+            Citizen.Wait(100)
+            if waiting > 5000 then
+            exports.pNotify:SendNotification({text = "CarCommand:\n Could not load the vehicle model in time, a crash was prevented.", layout = "centerLeft", theme = "relax", type = "error", timeout = math.random(1000, 10000)})
+
+                break
+            end
+        end
+        CreateVehicle(vehiclehash, x, y, z, GetEntityHeading(PlayerPedId())+90, 1, 0)
+    end)
+end)
